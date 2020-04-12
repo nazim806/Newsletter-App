f4633175eb6efe9dbc3d3d5b9396d4bb1636e9ac
Nazim Uddin <umnazim@gmail.com>
2020-04-12 04:01:43 -0700

    server runnig with post request

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, function () {
