80fb81aec1de99e200cc02b11f6624a0cce17ef3
Nazim Uddin <umnazim@gmail.com>
2020-04-12 03:37:14 -0700

    modified html and css files to fit our Project

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const request = require("request");
 
 const app = express();
 
+app.get("/", function (req, res) {});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
