0fa8af23bedae5f32334df0af5738df5bb7eb9a9
Nazim Uddin <umnazim@gmail.com>
2020-04-12 03:49:17 -0700

    relative path for staic files for the app to use it

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const request = require("request");
 
 const app = express();
 
-app.get("/", function (req, res) {});
+// to access static files like css and image we will use express and relative path to those files
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
